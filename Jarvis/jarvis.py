5edd291a2df9c2c66b532629b27e91ade800f84b
Alex Farrell <rfarrel2@uvm.edu>
2021-10-05 18:35:27 -0400

    Added release documentation to jarvis.py

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -1,3 +1,14 @@
 """
-    MAIN JARVIS CODE. THIS IS A PLACEHOLDER, EDIT AFTER.
+    Jarvis - A data-collecting chatbot for Slack
+    Current Release: 1.0
+
+    Jarvis is a chatbot designed for seamless integration with Slack 
+    workspaces. It will collect data from users and train itself 
+    to interact within the workspace.
+
+    Release notes for v.1.0:
+        - IN PROGRESS
+
+    This project is released as open-source under the permissive MIT 
+    license with the approval of its sponsor, Bagrow Industries.
 """
