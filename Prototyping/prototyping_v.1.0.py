cac863e2b1e7044856b1cc7be95be135ad4859fb
Alex Farrell <rfarrel2@uvm.edu>
2021-10-06 12:50:11 -0400

    Added instructions to prototyping file docstrings

--- Prototyping/prototyping_v.1.0.py
+++ Prototyping/prototyping_v.1.0.py
@@ -6,6 +6,10 @@
     before implementing into the main jarvis.py codebase. This file is
     just to help us test out certain things before jumping right in to 
     development.
+    
+    -------------------------------------------------------------------
+    Keep this file in the master branch as is without any edits. Create
+    a separate branch to overwrite this file with personal prototyping.
 """
 # Database management
 import sqlite3
