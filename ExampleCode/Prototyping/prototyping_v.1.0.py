95a21090303d6700bfd6bb90cca4461b7dc9d6a5
Alex Farrell <rfarrel2@uvm.edu>
2021-10-06 12:44:51 -0400

    Prepared prototyping file for testing Jarvis stuff before coming up with final design

--- ExampleCode/Prototyping/prototyping_v.1.0.py
+++ ExampleCode/Prototyping/prototyping_v.1.0.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Oct  6 12:33:20 2021
-
-@author: Alex Farrell
+    Jarvis - A data-collecting chatbot for Slack 
+    v.1.0 protoype
+    
+    All prototyping/testing code for Jarvis should go in this file
+    before implementing into the main jarvis.py codebase. This file is
+    just to help us test out certain things before jumping right in to 
+    development.
 """
+# Database management
+import sqlite3
+from sqlite3.dbapi2 import connect
 
+# Slack interaction
+import websocket
+try:
+    import thread 
+except ImportError:
+    import _thread as thread
+
+# Slack connection token
+from botsettings import API_TOKEN
