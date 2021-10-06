e2744e13fb38c97625a07901dd03b82ebb85a4af
Alex Farrell <rfarrel2@uvm.edu>
2021-10-06 14:38:08 -0400

    Test continued

---
+++ Prototyping/prototyping_v.1.0.py
@@ -0,0 +1,18 @@
+"""
+    Jarvis - A data-collecting chatbot for Slack 
+    v.1.0 protoype
+    
+    All prototyping/testing code for Jarvis should go in this file
+    before implementing into the main jarvis.py codebase. This file is
+    just to help us test out certain things before jumping right in to 
+    development.
+    
+    -------------------------------------------------------------------
+    Keep this file in the master branch as is without any edits. Create
+    a separate branch to overwrite this file with personal prototyping.
+"""
+# Add imports that will be used in jarvis.py
+
+if __name__ == '__main__':
+    # Add prototype code and run this file to test things
+    pass
