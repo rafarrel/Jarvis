7af5daa0084d58515eb2e9733a2cee667e19c30d
Alex Farrell <rfarrel2@uvm.edu>
2021-10-07 19:06:01 -0400

    Added APP token to example_botsettings.py and renamed ExampleCode directory to Examples

---
+++ Examples/JarvisExamples/example_botsettings.py
@@ -0,0 +1,15 @@
+"""
+    This is just here as an example of the botsettings.py file that we will
+    have on our computers. We don't want to upload this file to the repo
+    because it will contain the tokens which are used to allow Jarvis to
+    log in/connect to our workspace. Theoretically, if we uploaded the tokens
+    to the repo, then evil mysterious hackerZ could use it to log in to our
+    Slack repo and pursue their evil deeds!
+
+    What we will do instead is have this file be on our own computers with the
+    tokens so we can run Jarvis successfully without comprimising the security
+    of our Slack repo (and unleashing the wrath of Professor Bagrow to deduct
+    points). 
+"""
+API_TOKEN = "API TOKEN WOULD GO HERE -> starts with xoxb-"
+APP_TOKEN = "APP TOKEN WOULD GO HERE -> starts with xapp-"
