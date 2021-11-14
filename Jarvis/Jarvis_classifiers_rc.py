9535bd53b589fb7f713a8ef1fe853587485e8289
Erica Quallen <equallen@uvm.edu>
2021-11-14 14:07:34 -0500

    Added a lot of Coach Jarvis files, data, and code

---
+++ Jarvis/Jarvis_classifiers_rc.py
@@ -0,0 +1,118 @@
+# PROJECT 02 - PICKLING JARVIS
+# TEAM RED PIRANHA
+# COMPETITION 1
+
+##############################
+#          IMPORTS           #
+##############################
+
+import os
+import re
+import csv
+from string import punctuation
+import pandas as pd
+import numpy as  np
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from sklearn import ensemble
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+import pickle
+
+##############################
+#         FUNCTIONS          #
+##############################
+
+def performance_metrics(clf, X_test, y_test):
+    print('\nPERFORMANCE METRICS: ', type(clf))
+    print("Generating performance data...")
+    predictions = clf.predict(X_test)
+    cm = metrics.confusion_matrix(y_test, predictions)
+    print("\nConfusion Matrix:")
+    print(cm)
+    print("\nClassification Report:")
+    print(metrics.classification_report(y_test, predictions))
+    disp = metrics.ConfusionMatrixDisplay(cm)
+    disp.plot()
+    plt.show()
+
+
+def pickled_piranha(clf, directory, filename):
+    vec_clf = Pipeline([('vec', CountVectorizer()),
+                        ('tfidf', TfidfTransformer()),
+                        ('class', clf)])
+    vec_clf.fit(X_train, Y_train)
+    print("\nSaving classifier...")
+
+    try:
+        with open(os.path.join(os.getcwd(), directory, filename), 'wb') as file:
+            pickle.dump(vec_clf, file)
+    except:
+        os.mkdir(directory)
+        with open(os.path.join(os.getcwd(), directory, filename), 'wb') as file:
+            pickle.dump(vec_clf, file)
+            
+    path = os.path.join(os.getcwd(), directory, filename) + '.pkl'
+    with open(path, 'wb') as pickle_jar:
+        pickle.dump(vec_clf, pickle_jar)
+        
+
+
+##############################
+#         MAIN CODE          #
+##############################
+
+# Loads contents of data files into a master dictionary with key:value pairs TXT:ACTION 
+
+data = {}
+
+filepath = os.path.join(os.getcwd(), 'addiction_training_data', 'addiction-training-data.csv')
+with open(filepath, 'r') as f:
+    csvReader = csv.reader(f)
+    for row in csvReader:
+        if 'TXT' not in row:
+            data[row[0]] = row[1]
+        else:
+            pass
+
+
+X, Y = map(list, zip(*data.items()))
+
+# Instanatiates a CountVectorizer() object to run frequencies for every unique word in X 
+vectorizer = CountVectorizer()
+jarvis_vectorizer = vectorizer.fit_transform(X)
+jarvis_array = jarvis_vectorizer.toarray()
+jarvis_words = vectorizer.get_feature_names()
+
+# Split data into training and testing subsets
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+
+# Fitting Vectorizer to training and testing data, then sending to an array 
+
+trainX = vectorizer.fit_transform(X_train)
+trainX_array = trainX.toarray()
+testX = vectorizer.transform(X_test)
+testX_array = testX.toarray()
+
+
+################# TUNED MULTI-LAYER PERCEPTRON CLASSIFIER ########################
+
+print('********** Tuned Multi-Layer Perceptron Classifier Results *************')
+mlp_tuned = MLPClassifier(hidden_layer_sizes=400, solver = 'adam',
+                          shuffle = False, learning_rate = 'adaptive', activation = 'relu')
+mlp_tuned.fit(trainX_array, Y_train)
+performance_metrics(mlp_tuned, testX_array, Y_test)
+print('***********************************************************************')
+
+
+############################## PICKLING JARVIS ##################################
+pickled_piranha(mlp_tuned, 'Classifiers', 'jarvis_mlp_REDPIRANHA_recoverycoach')
--- Jarvis/jarvis_eda.py
+++ Jarvis/jarvis_eda.py
@@ -11,8 +11,13 @@ import re
 import json
 from string import punctuation
 import numpy as  np
+import pandas as pd
 from matplotlib import pyplot as plt
 from collections import Counter
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from Jarvis_classifiers import performance_metrics, trainX_array, testX_array, Y_train, Y_test
 
 ##############################
 #         FUNCTIONS          #
@@ -20,12 +25,12 @@ from collections import Counter
 
 def load_data(directory):
     "Loads contents of data files from the given directory into a  dictionary with key:value pairs ACTION:TXT"
-    data = {'PIZZA': [], 'JOKE': [], 'WEATHER': [], 'TIME': [], 'GREET': []}
-    one_line_list = []
+    data = {}
+    #one_line_list = []
     
-    original_files = os.listdir(os.path.join(os.getcwd(), directory))
+    files = os.listdir(os.path.join(os.getcwd(), directory))
     
-    for file in original_files:
+    for file in files:
         filepath = os.path.join(directory, file)
         with open(filepath, 'r') as f:
             if 'DS_Store' not in filepath:
@@ -33,22 +38,14 @@ def load_data(directory):
                     filetest = f.readline()
                     try:
                         if filetest[0] == "{":
-                            sub_dict = json.loads(line)
-                            temp = data[sub_dict['ACTION']]
-                            temp.append(sub_dict['TXT'].lower())
-                            data[sub_dict['ACTION']] = temp
+                            data['TXT'] = filetest['ACTION']
                         else:
-                            one_line_list.append(re.split(r',([A-Z]+)', filetest))
-                            temp = data[one_line_list[-1][1]]
-                            temp.append(one_line_list[-1][0])
-                            data[one_line_list[-1][1]] = temp
+                            temp = re.split(r',([A-Z]+)', filetest)
+                            data[temp[0]] = temp[1]
                     except:
                         pass
     return data
 
-
- 
-    
     
 ##############################
 #         MAIN CODE          #
@@ -57,18 +54,47 @@ def load_data(directory):
 #load dictionaries of original (provided) and cleaned external training data
 original_data = load_data('original_data')
 cleaned_data = load_data('training_data')
+pr01_data = load_data('PR01_data')
 
 #calculate counts of each ACTION
-original_counts = {}
-for key in original_data.keys():
-    original_counts[key] = len(original_data[key])
+original_counts = Counter(original_data.values())
+cleaned_counts = Counter(cleaned_data.values())  
 
-cleaned_counts = {}
-for key in cleaned_data.keys():
-    cleaned_counts[key] = len(cleaned_data[key])  
-    
+#get lists of keys and values for original data
+origX, origY = map(list, zip(*original_data.items()))
 
 
+#vectorize original data
+vectorizer = CountVectorizer()
+jarvis_vectorizer = vectorizer.fit_transform(origX)
+jarvis_array = jarvis_vectorizer.toarray()
+jarvis_words = vectorizer.get_feature_names()
+
+# Split data into training and testing subsets
+origX_train, origX_test, origY_train, origY_test = train_test_split(origX, origY)
+
+# Fitting Vectorizer to training and testing data, then sending to an array 
+trainX_orig = vectorizer.fit_transform(origX_train)
+origtrainX_array = trainX_orig.toarray()
+testX_orig = vectorizer.transform(origX_test)
+origtestX_array = testX_orig.toarray()
+
+
+#MLP for original data
+print('********** Original Multi-Layer Perceptron Classifier Results *************')
+mlp_orig = MLPClassifier(hidden_layer_sizes=400, solver = 'adam',
+                          shuffle = False, learning_rate = 'adaptive', activation = 'relu')
+mlp_orig.fit(origtrainX_array, origY_train)
+performance_metrics(mlp_orig, origtestX_array, origY_test)
+print('***********************************************************************')
+
+#MLP for cleaned data
+print('********** Cleaned Multi-Layer Perceptron Classifier Results *************')
+mlp_clean = MLPClassifier(hidden_layer_sizes=400, solver = 'adam',
+                          shuffle = False, learning_rate = 'adaptive', activation = 'relu')
+mlp_clean.fit(trainX_array, Y_train)
+performance_metrics(mlp_clean, testX_array, Y_test)
+print('***********************************************************************')
 
 
 
@@ -76,7 +102,7 @@ for key in cleaned_data.keys():
 #      VISUALIZATIONS        #
 ##############################
 
-
+#bar plot of counts of each ACTION for original vs. cleaned external data
 orig_count = plt.bar(original_counts.keys(), original_counts.values(), width = 0.3, align = 'edge', color = '#D81B60', label = 'Original Data')
 clean_count = plt.bar(cleaned_counts.keys(), cleaned_counts.values(), width = 0.3, align = 'center', color = '#1E88E5', label = 'Cleaned Data')
 plt.legend(loc = 'lower right')
